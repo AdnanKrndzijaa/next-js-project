43721580e9c3e30b82656624ff1fda6e4662c3c3
Adnan Krndzija <92021913+AdnanKrndzijaa@users.noreply.github.com>
2023-04-08 20:13:06 +0200

    404 page added

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import Link from "next/link"
-
+import { useRouter } from "next/router"
 
 function Home() {
+    const router = useRouter()
+
+    const handleClick = () => {
+        console.log("Placing your order")
+        router.push("/product")
+    }
     return (
         <div>
             <h1>Home page</h1>
@@ -11,6 +17,9 @@ function Home() {
             <Link legacyBehavior href="/product">
                 <a>Products</a>
             </Link>
+            <button onClick={handleClick}>
+                Place Order
+            </button>
         </div>
     )
 }
